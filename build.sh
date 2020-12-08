4ecd86bdc951898a8b9a22462f99eb75509a8b76
jasonjx-w <jasonjx.w@outlook.com>
2020-12-08 15:47:01 +0800

    [refactor]: env.sh/ build.sh / cmake * cmake support debug * refactor env.sh build.sh * refactor commit-template * refactor pre-commit
    Why:
    
    How:
    
    Log:
    * try to add clang-commit in git hook
    * but that will increases risk (such as modify *.sh or files of 3rdparty unexpected)
    * after some toss, just format files manually.
    
    Tags:
    
    See:

--- build.sh
+++ build.sh
@@ -4,20 +4,48 @@ set -e
 # env
 ##################################
 BUILD_DIR="build"
+BUILD_TYPE="Release"
 CMAKE="cmake"
 GCC=`which gcc`
 GXX=`which g++`
 
+# 2.build dir
 if [ ! -d ${BUILD_DIR} ]; then
   mkdir ${BUILD_DIR}
-else
-  rm -rf ${BUILD_DIR}/*
 fi
 
+# 3.handle build options
+if [ $# != 0 ]; then
+  while [ $# != 0 ]; do
+    case "$1" in
+      -d | --debug)
+        BUILD_TYPE="Debug"
+        shift
+        ;;
+      *)
+        echo "-- Unknown options ${1}, use -h or --help"
+        exit -1
+        ;;
+    esac
+  done
+fi
+
+# 4.force checking env.sh
+if [ ! ${OPS_BUILD_ENV_ENABLED} ]; then
+  echo "--Please source env.sh before build."
+  exit -1
+fi
+
+##################################
+# build
+##################################
 pushd ${BUILD_DIR}
+ rm -rf *
  ${CMAKE} \
+ -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
  -DCMAKE_C_COMPILE=${GCC} \
  -DCMAKE_CXX_COMPILER=${GXX} \
+ -DOPS_JUST_LIBRARY='OFF' \
  ..
 
  make -j32
